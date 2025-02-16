28da35b895d672c4ca626b01b072940409675c5b
duongbill <billduongg@gmail.com>
2025-02-16 23:19:26 +0700

    footer tim kiem

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import TrangChu from "./pages/home/TrangChu.js";
 import Login from "./pages/login/login.js";
@@ -12,28 +12,34 @@ import FooterComponents from "./components/footer/footer.js";
 import Filter from "./pages/filter/filter.js";
 import GenrePage from "./pages/genre/genre.js";
 import CountryPage from "./pages/country/country.js";
-
+import Page404 from "./pages/page404/page404.js";
+import Timkiem from "./pages/pagetk/timkiem.js";
 import Intro from "./pages/intro/intro.js";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Nav /> {/* Navbar is usually outside the Routes to be visible on all pages */}
-            <Routes>
-                <Route path="/" element={<Intro />} /> {/* Home Page */}
-                <Route path="/TrangChu" element={<TrangChu />} /> {/* Home Page */}
-                <Route path="/login" element={<Login />} />
-                <Route path="/film/:id1/:id2" element={<Film />} /> Assumed route for films listing
-                <Route path="/detail/:id" element={<MovieDetails />} /> {/* Route for individual film details */}
-                <Route path="/r/:listFilm" element={<Filter />} />
-                <Route path="/intro" element={<Intro />} />
-                <Route path="/genre" element={<GenrePage />} /> {/* Trang thể loại */}
-                <Route path="/country" element={<CountryPage />} /> {/* Trang thể loại */}
-                {/* You can add more routes here for other pages/components */}
-            </Routes>
-            <FooterComponents />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Nav />{" "}
+      {/* Navbar is usually outside the Routes to be visible on all pages */}
+      <Routes>
+        <Route path="/" element={<Timkiem />} /> {/* Home Page */}
+        <Route path="/TrangChu" element={<TrangChu />} /> {/* Home Page */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/film/:id1/:id2" element={<Film />} /> Assumed route for
+        films listing
+        <Route path="/detail/:id" element={<MovieDetails />} />{" "}
+        {/* Route for individual film details */}
+        <Route path="/r/:listFilm" element={<Filter />} />
+        <Route path="/intro" element={<Intro />} />
+        <Route path="/page404" element={<Page404 />} />
+        <Route path="/genre" element={<GenrePage />} /> {/* Trang thể loại */}
+        <Route path="/country" element={<CountryPage />} />{" "}
+        {/* Trang thể loại */}
+        {/* You can add more routes here for other pages/components */}
+      </Routes>
+      <FooterComponents />
+    </BrowserRouter>
+  );
 }
 
 export default App;
